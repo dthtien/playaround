2b2a1be673587116198127da528ba635c10c1dba
dthtien <tien.dau@employmenthero.com>
2023-04-04 09:23:57 +1000

    chore(fe): update config

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
-aws ec2-instance-connect send-ssh-public-key \
+aws ec3-instance-connect send-ssh-public-key \
     --instance-id i-0f6881991378b5e75 \
     --instance-os-user ubuntu \
     --ssh-public-key file://~/.ssh/id_rsa.pub
 
 docker compose run web db:prepare db:migrate
+
+aws ec2 authorize-security-group-ingress --group-id sg-023007be90edf21ee  --protocol tcp --port 3000 --cidr 202.65.91.170/32
+
